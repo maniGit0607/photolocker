e71865a31bcc766acf5db8e28d80bae89a291684
Manikandan Kannan <manikandan.kannan@gendigital.com>
2025-11-15 22:58:47 +0530

    upgrade material 3

--- app/src/main/java/com/photovault/locker/activities/PhotoViewActivity.kt
+++ app/src/main/java/com/photovault/locker/activities/PhotoViewActivity.kt
@@ -1,10 +1,7 @@
 package com.photovault.locker.activities
 
-import android.os.Build
 import android.os.Bundle
 import android.view.View
-import android.view.WindowInsets
-import android.view.WindowInsetsController
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
@@ -228,38 +225,5 @@ class PhotoViewActivity : AppCompatActivity() {
         }
     }
     
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-        if (hasFocus) {
-            // Hide system UI for immersive experience using modern API
-            hideSystemUI()
-        }
-    }
-    
-    private fun hideSystemUI() {
-        // Enable edge-to-edge only for this activity
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            // API 30+ using WindowInsetsController
-            window.insetsController?.let { controller ->
-                controller.hide(
-                    WindowInsets.Type.statusBars() 
-                    or WindowInsets.Type.navigationBars()
-                )
-                controller.systemBarsBehavior = 
-                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-            }
-        } else {
-            // Fallback for API 29 and below (though your minSdk is 30)
-            @Suppress("DEPRECATION")
-            window.decorView.systemUiVisibility = (
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_FULLSCREEN
-            )
-        }
-    }
 }
 
